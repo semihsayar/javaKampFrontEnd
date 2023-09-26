bc2b72e52e83068935f6f4727175ee307a73c874
sayar <sayars244@gmail.com>
2023-09-26 15:07:50 +0300

    kampJavaFrontEnd

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Categories from "./Categories";
+import ProductList from "../pages/ProductList";
+import { Grid, GridColumn, GridRow } from 'semantic-ui-react'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Grid>
+        <GridRow>
+          <GridColumn width={4}><Categories/></GridColumn>
+          <GridColumn width={12}><ProductList/></GridColumn>
+        </GridRow>
+      </Grid>
+    </div>
+    
+   
+  );
+}
